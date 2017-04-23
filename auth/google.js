88d38032dc5d403e8c7d62c0bec5b399a18bfdc9
CJ R <hello@cjr.co.de>
2017-04-23 13:07:40 -0600

    :bug: working on deploy

--- auth/google.js
+++ auth/google.js
@@ -7,7 +7,7 @@ const { User } = require('../models');
 const Strategy = new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
 }, (accessToken, refreshToken, profile, cb) => {
   console.log(profile);
   if (profile.email && profile.email.match(/@galvanize.com/g)) {
